ef84395a5ff7a704fd70719a50e8a55db75eb5bf
yuvalg <yuval.gabay@wsc-sports.com>
2019-10-10 12:45:02 +0300

    changed API to webhook_url and 'text' field in data

--- toaster/__init__.py
+++ toaster/__init__.py
@@ -3,14 +3,13 @@ import datetime
 
 from toaster.helper import get_config_path, load_default_id, format_time
 
-USERID = load_default_id()
-API_ENDPOINT = 'https://us-central1-toaster-253815.cloudfunctions.net/toaster_message'
+webhook_url = load_default_id()
 
 def toast(method):
-    if USERID == '':
-        raise UnboundLocalError('You have not configured your Telegram ID. Run set_id(<telegram id>) first.')
+    if webhook_url == '':
+        raise UnboundLocalError('You have not configured your Telegram ID. Run set_incoming_webhook(<telegram id>) first.')
 
-    def insert_toast(*args, **kw):      
+    def insert_toast(*args, **kw):
         try:
             start = datetime.datetime.now()
             result = method(*args, **kw)
@@ -21,35 +20,27 @@ def toast(method):
                 method.__name__, start.strftime("%Y-%m-%d %H:%M:%S"), end.strftime("%Y-%m-%d %H:%M:%S"), format_time(diff)
             )
             data = {
-                'userid':USERID,
-                'msg':msg
+                'text':msg
             }
 
-            res = requests.post(url = API_ENDPOINT, data = data)
+            res = requests.post(url = WEBHOOK_URL, data = data, headers={'Content-Type': "application/json"})
             return result
 
         except Exception as e:
             msg = '⚠️ An error has occurred with function <i>{}</i>\n<b>Error message:</b> {}'.format(method.__name__, str(e))
             print('Error caught:',e)
             data = {
-                'userid':USERID,
-                'msg':msg
+                'text':msg
             }
             # Send Telegram Message
-            res = requests.post(url = API_ENDPOINT, data = data)
+            res = requests.post(url = WEBHOOK_URL, data = data, headers={'Content-Type': "application/json"})
 
     return insert_toast
 
-def set_id(userid):
-    userid = str(userid)
-    if userid.isnumeric():
-        config_path = get_config_path()
-        with open(config_path,"w") as config_file:
-            config_file.write(userid)
-        global USERID
-        USERID = userid
-    else:
-        raise TypeError('Your Telegram ID should be all numbers.')
-
-
-
+def set_incoming_webhook(webhook_url):
+    webhook_url = str(webhook_url)
+    config_path = get_config_path()
+    with open(config_path,"w") as config_file:
+        config_file.write(webhook_url)
+    global WEBHOOK_URL
+    WEBHOOK_URL = webhook_url
