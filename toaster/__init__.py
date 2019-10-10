b0e382bf7f41b7455a1ace97f2e5947928c65df6
yuvalg <yuval.gabay@wsc-sports.com>
2019-10-10 13:35:45 +0300

    1 . Slack Adaptetion message formatting 2. import json as slack requiers json.dumps payload 3. changed error message to match slack

--- toaster/__init__.py
+++ toaster/__init__.py
@@ -1,13 +1,13 @@
 import os, requests
 import datetime
-
+import json
 from toaster.helper import get_config_path, load_default_id, format_time
 
 webhook_url = load_default_id()
 
 def toast(method):
     if webhook_url == '':
-        raise UnboundLocalError('You have not configured your Telegram ID. Run set_incoming_webhook(<telegram id>) first.')
+        raise UnboundLocalError('You have not configured your webhook url. Run set_incoming_webhook(<webhook url>) first.')
 
     def insert_toast(*args, **kw):
         try:
@@ -16,18 +16,18 @@ def toast(method):
             end = datetime.datetime.now()
             diff = end - start
             # Create Message
-            msg = "🍞 Ding! Function <i>{}</i> has completed!\n<b>Start Time:</b> {}\n<b>End Time:</b> {}\n<b>Time Taken:</b> {}".format(
+            msg = "🍞 Ding! Function *{}* has completed!\n*Start Time:* {}\n*End Time:* {}\n*Time Taken:* {}".format(
                 method.__name__, start.strftime("%Y-%m-%d %H:%M:%S"), end.strftime("%Y-%m-%d %H:%M:%S"), format_time(diff)
             )
             data = {
                 'text':msg
             }
 
-            res = requests.post(url = WEBHOOK_URL, data = data, headers={'Content-Type': "application/json"})
+            res = requests.post(url = WEBHOOK_URL, data = json.dumps(data), headers={'Content-Type': "application/json"})
             return result
 
         except Exception as e:
-            msg = '⚠️ An error has occurred with function <i>{}</i>\n<b>Error message:</b> {}'.format(method.__name__, str(e))
+            msg = '⚠️ An error has occurred with function *Error message:* {}'.format(method.__name__, str(e))
             print('Error caught:',e)
             data = {
                 'text':msg
